20bed2307c680cffec2934aaf5b09ea3ef72729c
John Raymund Bernal <bernaljohnraymund777@gmail.com>
2021-01-13 15:23:27 +0800

    test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-	it("respond with Hello World", (done) => {
-		request(app).get("/").expect("Hi there!", done);
-	})
+  it("respond with Hello World", (done) => {
+	request(app).get("/").expect("Hello World", done);
+  })
 });
